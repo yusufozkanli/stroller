627057cb5e4e07cf8ffb6e3c5add4475afdf999c
yusuf ozkanli <myozkanli@gmail.com>
2017-12-10 22:09:14 +0000

    routescreated

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,14 @@ class PagesController < ApplicationController
 
   def home
   end
+
+  def dashboard
+    if current_user.admin?
+      @user = current_user
+      @users = User.all
+      @users_count = User.count
+    else
+      redirect_to root_path
+    end
+  end
 end
